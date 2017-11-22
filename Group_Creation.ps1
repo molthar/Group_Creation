d97573510635fb26f9302ad434411f5006e6b8d7
Michael Pruhs <michael.pruhs@cui.edu>
2017-11-22 12:42:21 -0800

    Started on function to make distribution group. added don't allow external and owners can invite

--- Group_Creation.ps1
+++ Group_Creation.ps1
@@ -1,7 +1,24 @@
+
+
 Function Create_Group {
 	param (
 		[string] $groupname
 )
 	gam create group `
 	$groupname `
+}
+
+Function Make_Distribution_Group {
+	param (
+		[string] $groupname
+	)
+		gam update group `
+		$groupname `
+
+		allow_external_members `
+		FALSE `
+
+		who_can_invite `
+		owners_can_invite `
+
 }
